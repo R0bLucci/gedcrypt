52ec9f21f9c0094b52afb0517ed0f130f695ad51
Kyle Bagshaw <rkbagshaw10@gmail.com>
2015-10-11 02:30:39 +0100

    edit command line arg checking code and add comments

--- edcrypt.py
+++ edcrypt.py
@@ -1,14 +1,21 @@
-import crypto
 import os
 import sys
+
+import crypto
 
 def fail_exit():
 	print('Usage: python edcrypt.py -e(encrypt)/-d(decrypt) filename password')
 	raise SystemExit(1)
 
-if len(sys.argv) != 4 or not os.path.isfile(sys.argv[2]):
+# check command line args are valid
+if len(sys.argv) != 4:
 	fail_exit()
 
+if not os.path.isfile(sys.argv[2]):
+	print('File ' + sys.argv[2] + ' does not exist.')
+	raise SystemExit(1)
+
+# parse command line args
 if sys.argv[1] == '-e':
 	crypto.encrypt_file(sys.argv[3].encode(), sys.argv[2])
 elif sys.argv[1] == '-d':
